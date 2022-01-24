b5834d1e09f790121cec71bcd9b5605d8fe3ee60
Joelson Júnior <70169714+jlsnjnr@users.noreply.github.com>
2022-01-24 16:14:25 -0300

    first commit

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,27 @@
+export const theme = {
+  colors: {
+    primary: '#E51C44',
+    yellow: '#FECE51',
+  
+    bg: '#1B1925',
+    bgSecond: '#252528',
+    
+    textButtonBlack: '#1B1925',
+    blackSecondary: '#252332',
+   
+    overlay: 'rgba(0,0,0,0.7)',
+    highlight: '#ABB1CC',
+    heading: '#DDE3F0',
+    line: '#991F36',
+    on: '#32BD50',
+
+    discord: '#7289da',
+  },
+
+  fonts: {
+    title700: 'Rajdhani_700Bold',
+    title500: 'Rajdhani_500Medium',
+    text400: 'Inter_400Regular',
+    text500: 'Inter_500Medium',
+  }
+};
---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { 
+  View, 
+  Text, 
+  Image,
+  Alert,
+  ActivityIndicator,
+  TextInput
+} from 'react-native';
+import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
+
+
+import { useAuth } from '../../hooks/auth';
+
+import IllustrationImg from '../../assets/illustration.png';
+import { theme } from '../../global/styles/theme';
+import { styles } from './styles';
+
+import { Background } from '../../components/Background';
+
+export function SignIn(){
+
+  return(
+    <Background>
+      <View style={styles.container}>     
+        <Image 
+          source={IllustrationImg} 
+          style={styles.image} 
+          resizeMode="cover"
+        />
+
+        <View style={styles.content}>
+          <Text style={styles.title}>
+            Bem-vindo {'\n'}
+            a Romtec
+          </Text>
+
+          <RectButton 
+            style={styles.containerButton} 
+          >
+            <Text style={styles.titleButton}>
+              Entrar
+            </Text>
+          </RectButton>
+          <RectButton 
+            style={styles.containerButtonRegister} 
+          >
+            <Text style={styles.titleButtonRegister}>
+              Cadastrar
+            </Text>
+          </RectButton>
+                              
+        </View>
+      </View>
+    </Background>
+  );
+}
